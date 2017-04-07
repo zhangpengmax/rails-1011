225391605811356459c693a785e7351b41a7fb60
NewMax <zhangpengmax@gmail.com>
2017-04-06 22:08:06 -0400

    implement groups#create

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,35 +3,21 @@ class GroupsController < ApplicationController
     @groups = Group.all
   end
 
- def new
-   @group = Group.new
+  def new
+    @group = Group.new
+  end
+
+ def create
+   @group = Group.new(group_params)
+   @group.save
+
+     redirect_to groups_path
  end
+
+ private
+
+ def group_params
+   params.require(:group).permit(:title, :description)
+ end
+
 end
-
-   def show
-     @group = Group.find(params[:id])
-   end
-
-   def edit
-     @group = Group.find(params[:id])
-   end
-
-   def new
-     @group = Group.new
-   end
-
-
-
-
-      def destroy
-        @group = Group.find(params[:id])
-        @group.destroy
-        flash[:alert] = "Group deleted"
-        redirect_to groups_path
-      end
-
-    private
-
-    def group_params
-      params.require(:group).permit(:title, :description)
-    end

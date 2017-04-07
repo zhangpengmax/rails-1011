c1652d64b64d284d65fb6faf68394e9b65d06819
NewMax <zhangpengmax@gmail.com>
2017-04-06 22:06:35 -0400

    implement groups#new

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,6 +3,11 @@ class GroupsController < ApplicationController
     @groups = Group.all
   end
 
+ def new
+   @group = Group.new
+ end
+end
+
    def show
      @group = Group.find(params[:id])
    end
@@ -15,25 +20,8 @@ class GroupsController < ApplicationController
      @group = Group.new
    end
 
-   def create
-     @group = Group.new(group_params)
 
-     if @group.save
-   redirect_to groups_path
- else
-   render :new
- end
-end
 
-    def update
-      @group = Group.find(params[:id])
-      @group.update(group_params)
-      
-      redirect_to groups_path, notice: "Update Success"
-    else
-      render :edit
-    end
-  end
 
       def destroy
         @group = Group.find(params[:id])
@@ -47,5 +35,3 @@ end
     def group_params
       params.require(:group).permit(:title, :description)
     end
-
-  end

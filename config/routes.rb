ea10f85cd243b16ff06b6819083c414a5351f9d6
NewMax <zhangpengmax@gmail.com>
2017-04-07 20:32:10 -0400

    implement my groups

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :groups do
-
         member do
           post :join
           post :quit
@@ -9,6 +8,10 @@ Rails.application.routes.draw do
 
         resources :posts
       end
-      
+
+  namespace :account do
+     resources :groups
+end
+
   root 'groups#index'
 end
